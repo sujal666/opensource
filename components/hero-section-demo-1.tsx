9e79a64d1c3d608e050d04f87cf8d2f45063b788
Sujal <poojarisujal03@gmail.com>
2025-06-22 22:40:46 +0530

    landing page intial setup

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -2,8 +2,6 @@
 
 
 import { motion } from "motion/react";
-import { FeatureBentoGrid } from "./_components/FeatureBentoGrid";
-import Link from "next/link";
 
 export default function HeroSectionOne() {
   return (
@@ -68,12 +66,12 @@ export default function HeroSectionOne() {
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
-          <Link href={'/dashboard'} >
           <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
             Explore Now
           </button>
-          </Link>
-
+          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
+            Contact Support
+          </button>
         </motion.div>
         <motion.div
           initial={{
@@ -101,7 +99,6 @@ export default function HeroSectionOne() {
           </div>
         </motion.div>
       </div>
-      <FeatureBentoGrid />
     </div>
   );
 }
